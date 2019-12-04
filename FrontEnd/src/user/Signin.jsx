4a55f6bfc9bd2ba4fd13445245c6f2170adeb676
thaparahul <connectthapa84@gmail.com>
2019-12-03 22:19:43 -0500

    added some front end files

---
+++ FrontEnd/src/user/Signin.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Layout from '../core/Layout';
+
+const Signin = () => (
+    <Layout title="Signin" description="Sign in to Node React VuShares App">
+    ...
+    </Layout>
+);
+
+export default Signin;
---
+++ FrontEnd/src/user/Signup.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Layout from '../core/Layout';
+import {API} from '../config';
+const Signup = () => ( 
+    <Layout title = "Signup" description = "Signup to Node React VuShares App" >
+        { API } 
+    </Layout>
+);
+
+export default Signup;
